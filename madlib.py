8f066b159e6bb7f49c6e960cfd8a31418a62701f
Armoghan <armoghanshaheen@hotmail.com>
2022-02-22 14:06:04 -0700

    first three basic python projects

---
+++ madlib.py
@@ -0,0 +1,9 @@
+adj=input("Adjective= ")
+verb1=input("Verb= ")
+verb2=input("Verb= ")
+celeb=input("Celebrity: ")
+
+madlib=f"Computer programming is so {adj}! it make me so excited all the time because\
+    I love to {verb1}. Stay hydrated and {verb2} like you are {celeb}!"
+    
+print(madlib)
