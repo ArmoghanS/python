280189247d2d73e16e3c08629c42eb7eb143373c
Armoghan <armoghanshaheen@hotmail.com>
2022-02-27 17:39:05 -0700

    start and exit buttons added, along with game lgoic

--- RPS_game/main.py
+++ RPS_game/main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 pygame.init()
@@ -12,7 +13,8 @@ bg_img=pygame.transform.scale(bg_img,(WIDTH,HEIGHT))
 rock_img=pygame.transform.scale(pygame.image.load('RPS_game/Assets/Rock.png'),(100,100))
 paper_img=pygame.transform.scale(pygame.image.load('RPS_game/Assets/Paper.png'),(100,100))
 scissors_img=pygame.transform.scale(pygame.image.load('RPS_game/Assets/scissors.png'),(100,100))
-
+start_img=pygame.transform.scale(pygame.image.load('RPS_game/Assets/start.png'),(120,80))
+exit_img=pygame.transform.scale(pygame.image.load('RPS_game/Assets/exit.png'),(120,80))
 
 
 
@@ -22,9 +24,22 @@ class Button():
         self.image=image
         self.rect=self.image.get_rect()
         self.rect.topleft=(x,y)
+        self.clicked=False
     
     def draw(self):
+        action=False
+        pos = pygame.mouse.get_pos()
+        
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == True and self.clicked==False:
+                self.clicked=True
+                action=True
+                
+            if pygame.mouse.get_pressed()[0]==False:
+                self.clicked=False
+        
         WIN.blit(self.image,(self.rect.x,self.rect.y))
+        return action
 
 ################################ END BUTTON CLASS ################################
 
@@ -32,13 +47,18 @@ class Button():
 rock=Button(150,200,rock_img)
 paper=Button(400,200,paper_img)
 scissors=Button(650,200,scissors_img)
+start_btn=Button(450,400,start_img)
+exit_btn=Button(700,20,exit_img)
 
+
+#Variables
 FPS=60
-def game():
-    #rock=pygame.Rect(150,200,100,100)
-    #paper=pygame.Rect(400,200,100,100)
-    #scissors=pygame.Rect(650,200,100,100)
-    
+player=""
+computer=random.choice(['r','p','s'])
+
+
+
+def game():   
     
     
     clock=pygame.time.Clock()
@@ -56,15 +76,33 @@ def game():
     quit()
 
 
+def play(player,opponent):
+    if player==computer:
+        print("Tie")
+        pygame.quit()
+        quit()
+    if (player== 'r' and opponent=='s') or(player=='s' and opponent=='p') or (player=='p'and opponent=='r'):
+        return True
+    
+
 #Drawing thigns onto screen
 def bg_update():
-    WIN.blit(bg_img,(0,0))
-    #WIN.blit(rock_img,(150,200))
-    #WIN.blit(paper_img,(400,200))
-    #WIN.blit(scissors_img,(650,200))
-    rock.draw()
-    paper.draw()
-    scissors.draw()
+    WIN.blit(bg_img,(0,0)) #Draws Background
+    if rock.draw():
+        print("You've Chosen Rock")
+        player='r'
+    if paper.draw():
+        print("You've Chosen Paper")
+        player='p'
+    if scissors.draw():
+        print("You've Chosen Scissors")
+        player='s'
+    if exit_btn.draw():
+        pygame.quit()
+        quit()
+    if start_btn.draw():
+        if play(player,computer):
+            print("You've won")
     pygame.display.update()   
             
         
